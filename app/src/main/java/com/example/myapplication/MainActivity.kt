94c47efd186a3cfe8a84c0e0708d2bb20170e797
taharahonoka <wohonota@gmail.com>
2023-09-27 15:40:38 +0900

    [add]ダイアログ実装

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +27,21 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
+
+        val dialogButton = findViewById<Button>(R.id.dialog_button)
+        dialogButton.setOnClickListener {
+            AlertDialog.Builder(this).setTitle("ダイアログのタイトルをいれう")
+                .setPositiveButton("OK", { dialog, which ->
+                    //OKの時の処理
+                    val intent = Intent(this, WebViewActivity::class.java)
+                    startActivity(intent)
+
+                }).setNegativeButton("No", { dialog, which ->
+                    //NOの時の処理
+                    finish()
+                }).show()
+        }
+
 
     }
 }
